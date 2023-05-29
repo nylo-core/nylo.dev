14d842cbc48cde3567d2d9da188f7de8eed2ed72
Anthony <ants52@aol.com>
2023-05-29 12:56:52 +0100

    feat: version bump

--- config/project/meta.php
+++ config/project/meta.php
@@ -3,17 +3,17 @@
 return [
     'repos' => [
         'nylo' => [
-            'version' => 'v5.0.2',
+            'version' => 'v5.0.3',
             'repo_name' => 'http://github.com/nylo-core/nylo'
         ],
 
         'framework' => [
-            'version' => 'v5.0.1',
+            'version' => 'v5.0.2',
             'repo_name' => 'http://github.com/nylo-core/framework'
         ],
 
         'support' => [
-            'version' => 'v5.1.1',
+            'version' => 'v5.1.2',
             'repo_name' => 'http://github.com/nylo-core/support'
         ],
     ],
