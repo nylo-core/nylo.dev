1a2ca244259391fa7e1868882b873a1bb185ceee
Anthony <ants52@aol.com>
2024-02-28 19:51:06 +0700

    fix: ecosystem blade view

--- resources/views/pages/ecosystem.blade.php
+++ resources/views/pages/ecosystem.blade.php
@@ -15,19 +15,19 @@
 		<div class="hover:bg-gray-50 p-[24px] rounded border border-2 border-gray-100">
 				<span class="font-semibold text-[20px]">{{ config('app.name') }} Permission Policy</span>
 
-				<span class="bg-[#ECF5FC] ml-2 px-2 text-primary-blue-deep rounded-3xl py-1 font-semibold">{{ $resourceData['permission_policy']['version'] }}</span>
+				<span class="bg-[#ECF5FC] ml-2 px-2 text-primary-blue-deep rounded-3xl py-1 font-semibold">{{ $resourceData['permission-policy']['version'] }}</span>
 
 				<p class="text-[18px] text-[#81888E] mb-2 mt-2" style="letter-spacing: -0.02em;">Handle roles and permissions in your Flutter app.</p>
 
 				<div class="flex flex-wrap">
 
-					<a href="{{ $resourceData['permission_policy']['repository_url'] }}" target="_BLANK" class="inline-flex self-center text-[#6C7379]">
+					<a href="{{ $resourceData['permission-policy']['repository_url'] }}" target="_BLANK" class="inline-flex self-center text-[#6C7379]">
                         View <img src="{{ asset('images/upper_right_arrow.png') }}" class="h-[20px] w-[20px] self-center">
 				</a>
 
 				<img src="{{ asset('images/rectangle.png') }}" class="w-0.5 mx-2 h-5 m-auto self-center">
 
-				<a href="{{ $resourceData['permission_policy']['release_note_url'] }}" target="_BLANK" class="inline-flex self-center text-[#6C7379]">
+				<a href="{{ $resourceData['permission-policy']['release_note_url'] }}" target="_BLANK" class="inline-flex self-center text-[#6C7379]">
 					Release Notes <img src="{{ asset('images/upper_right_arrow.png') }}" class="h-[20px] w-[20px] self-center">
 				</a>
 				</div>
@@ -36,18 +36,18 @@
         <div class="hover:bg-gray-50 p-[24px] rounded border border-2 border-gray-100">
 				<span class="font-semibold text-[20px]">Media Pro</span>
 
-				<span class="bg-[#ECF5FC] ml-2 px-2 text-primary-blue-deep rounded-3xl py-1 font-semibold">{{ $resourceData['media_pro']['version'] }}</span>
+				<span class="bg-[#ECF5FC] ml-2 px-2 text-primary-blue-deep rounded-3xl py-1 font-semibold">{{ $resourceData['media-pro']['version'] }}</span>
 
 				<p class="text-[18px] text-[#81888E] mb-2 mt-2" style="letter-spacing: -0.02em;">Package for handling Media in your Flutter project.</p>
 
 				<div class="flex flex-wrap">
-					<a href="{{ $resourceData['media_pro']['repository_url'] }}" class="inline-flex self-center text-[#6C7379]">
+					<a href="{{ $resourceData['media-pro']['repository_url'] }}" class="inline-flex self-center text-[#6C7379]">
 					View <img src="{{ asset('images/upper_right_arrow.png') }}" class="h-[20px] w-[20px] self-center">
 				</a>
 
 				<img src="{{ asset('images/rectangle.png') }}" class="w-0.5 mx-2 h-5 m-auto self-center">
 
-                <a href="{{ $resourceData['media_pro']['release_note_url'] }}" target="_BLANK" class="inline-flex self-center text-[#6C7379]">
+                <a href="{{ $resourceData['media-pro']['release_note_url'] }}" target="_BLANK" class="inline-flex self-center text-[#6C7379]">
 					Release Notes <img src="{{ asset('images/upper_right_arrow.png') }}" class="h-[20px] w-[20px] self-center">
 				</a>
 				</div>
