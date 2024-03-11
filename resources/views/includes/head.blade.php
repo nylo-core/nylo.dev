afbe42412705720f4bb9b6caa41a204a6f9132f9
Anthony <ants52@aol.com>
2024-03-11 14:53:03 +0700

    update seo

--- resources/views/includes/head.blade.php
+++ resources/views/includes/head.blade.php
@@ -3,10 +3,9 @@
 
   <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.5">
   <meta name="author" content="{{ config('app.name') }}" />
-  {!! SEO::generate() !!}
-
+  {!! SEO::generate(true) !!}
+  <meta name="twitter:image" content="{{ asset('images/nylo_logo.png') }}">
   <link href="{{ asset('css/remixicon' . (config('app.env') == 'production' ? '.min' : '') . '.css') }}" rel="stylesheet">
-
   <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('apple-touch-icon.png') }}">
   <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('favicon-32x32.png') }}">
   <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('favicon-16x16.png') }}">
